3866cca17bf106d1377dc5e955912a7467fbe7dc
Sam7898 <117601367+Sam7898@users.noreply.github.com>
2023-02-25 17:29:57 -0600

    Add files via upload
    Signed-off-by: Sam7898 <117601367+Sam7898@users.noreply.github.com>

--- kwitter.js
+++ kwitter.js
@@ -1,3 +1,15 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA2UEoqjMbw5Huyhqx8RDKT01h30rz238c",
+    authDomain: "lets-chat-web-app-41f72.firebaseapp.com",
+    projectId: "lets-chat-web-app-41f72",
+    storageBucket: "lets-chat-web-app-41f72.appspot.com",
+    messagingSenderId: "268387694245",
+    appId: "1:268387694245:web:7d33181348add7fdb7bf4c"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 function addUser(){
     user_name = document.getElementById("user_name").value;
     localStorage.setItem("user_name", user_name);
---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA2UEoqjMbw5Huyhqx8RDKT01h30rz238c",
+    authDomain: "lets-chat-web-app-41f72.firebaseapp.com",
+    projectId: "lets-chat-web-app-41f72",
+    storageBucket: "lets-chat-web-app-41f72.appspot.com",
+    messagingSenderId: "268387694245",
+    appId: "1:268387694245:web:7d33181348add7fdb7bf4c"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
